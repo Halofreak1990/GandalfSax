8150a46e96d0674403015a9cc041b00aba94615a
Tom Lint <halofreak_1990@hotmail.com>
2021-07-03 01:50:52 +0200

    Removed unused usings and references

--- GandalfSax/App.xaml.cs
+++ GandalfSax/App.xaml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Drawing;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using System.Windows;
+using System.Windows.Media;
+
 using Application = System.Windows.Application;
 
 namespace GandalfSax
