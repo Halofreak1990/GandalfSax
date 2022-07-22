33be728fa74f3e32f5f43a76e82c48e2da1cde84
Tom Lint <halofreak_1990@hotmail.com>
2022-07-22 23:37:12 +0200

    Added event handlers to auxiliary screens to allow any screen to exit the screensaver

--- GandalfSax/SecondaryWindow.xaml.cs
+++ GandalfSax/SecondaryWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace GandalfSax
@@ -8,11 +9,38 @@ namespace GandalfSax
 	/// </summary>
 	public partial class SecondaryWindow : Window
 	{
+		private Point _lastMousePosition = default(Point);
+
 		public SecondaryWindow(VisualBrush visualBrush)
 		{
 			InitializeComponent();
 
 			Display.Fill = visualBrush;
+		}
+
+		private void OnSecondaryWindowKeyDown(object sender, KeyEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void OnSecondaryWindowMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void OnSecondaryWindowMouseMove(object sender, MouseEventArgs e)
+		{
+			var pos = e.GetPosition(this);
+
+			if (_lastMousePosition != default(Point))
+			{
+				if ((_lastMousePosition - pos).Length > 3)
+				{
+					Application.Current.Shutdown();
+				}
+			}
+
+			_lastMousePosition = pos;
 		}
 	}
 }
